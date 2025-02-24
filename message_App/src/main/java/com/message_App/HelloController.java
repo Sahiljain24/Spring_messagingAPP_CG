de6e2071d868ff8555e0a956a85c47f09a84283c
Sahiljain24 <24sahilj@gmail.com>
2025-02-24 15:03:44 +0530

    [Sahil] Add. added uc_02

--- message_App/src/main/java/com/message_App/HelloController.java
+++ message_App/src/main/java/com/message_App/HelloController.java
@@ -1,9 +1,7 @@
 package com.message_App;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -13,5 +11,11 @@ public class HelloController {
     public String greet(){
         return "Hello from BridgeLabs";
     }
+    @GetMapping("/hello/querry")
+    public String greet(@RequestParam String name ){
+        return "Hello " +name + " from BridgeLabs";
+    }
+
+   
 
 }
